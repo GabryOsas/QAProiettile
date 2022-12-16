e3ade702d253aedd6b1adf7c2a458907cd749303
OSAS50 <82862979+OSAS50@users.noreply.github.com>
2022-12-16 21:53:42 +0100

    Costum Model Data update

---
+++ src/me/gabryosas/commands/BisturiGive.java
@@ -0,0 +1,34 @@
+package me.gabryosas.commands;
+
+import me.gabryosas.Main;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class BisturiGive implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            if (player.hasPermission(Main.plugin.getConfig().getString("Permission.BisturiPerms"))){
+                ItemStack itemStack = new ItemStack(Material.valueOf(Main.plugin.getConfig().getString("QAProiettile.Material")));
+                ItemMeta itemMeta = itemStack.getItemMeta();
+                itemMeta.setDisplayName(ChatColor.translateAlternateColorCodes('&', Main.plugin.getConfig().getString("QAProiettile.Bisturi")));
+                itemMeta.setCustomModelData(Main.plugin.getConfig().getInt("QAProiettile.CostumModelData"));
+                itemStack.setItemMeta(itemMeta);
+                player.getInventory().addItem(itemStack);
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&', Main.plugin.getConfig().getString("Message.Give")));
+            } else {
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&', Main.plugin.getConfig().getString("Message.NoPerms")));
+            }
+        }else {
+            Main.plugin.getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', Main.plugin.getConfig().getString("Message.AntiConsole")));
+        }
+        return true;
+    }
+}
